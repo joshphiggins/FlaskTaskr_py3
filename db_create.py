302c11fe6ee7ee2a0da8bf5bc1a1cac3dfa1ba7d
Josh Higgins <joshphiggins@gmail.com>
2016-06-13 15:47:00 -0400

    fourth

--- db_create.py
+++ db_create.py
@@ -1,20 +1,10 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-	c = connection.cursor()
-	c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY 
-		AUTOINCREMENT,
-		name TEXT NOT NULL, due_date TEXT NOT NULL, priority
-			INTEGER NOT NULL,
-		status INTEGER NOT NULL)""")
+db.create_all()
 
-	c.execute(
-		'INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES ("Finish this tutorial", "07/12/2016", 10, 1)'
-		)
+db.session.add(Task("Finish this tutorial", date(2016, 7, 12), 10, 1))
+db.session.add(Task("Finish Real Python Book 2", date(2016, 7, 12), 10, 1))
 
-	c.execute(
-		'INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish Real Paython Course 2", "07/12/2016", 10, 1)'
-		)
+db.session.commit()
