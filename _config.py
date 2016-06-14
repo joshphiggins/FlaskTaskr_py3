2fe0a5946a8970bec4bafbb7535f7421f5f2cdf6
Josh Higgins <joshphiggins@gmail.com>
2016-06-14 10:31:40 -0400

    5

--- _config.py
+++ _config.py
@@ -3,8 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 CSRF_ENABLED = True
 SECRET_KEY = 'R\\D\x019\x03N\x82\x1c\x04z\xc6\x9c\x9f\x9f\xf5U\xae\xb8\xb0\r\xb9[\xf1'
 
--- db_create.py
+++ db_create.py
@@ -4,7 +4,7 @@ from datetime import date
 
 db.create_all()
 
-db.session.add(Task("Finish this tutorial", date(2016, 7, 12), 10, 1))
-db.session.add(Task("Finish Real Python Book 2", date(2016, 7, 12), 10, 1))
+#db.session.add(Task("Finish this tutorial", date(2016, 7, 12), 10, 1))
+#db.session.add(Task("Finish Real Python Book 2", date(2016, 7, 12), 10, 1))
 
 db.session.commit()
